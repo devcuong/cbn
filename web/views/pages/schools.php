367d822bfea85e853693ac4653dcf45299509f69
devcuong <viettxt@gmail.com>
2020-07-11 23:41:13 +0700

    update

--- web/views/pages/schools.php
+++ web/views/pages/schools.php
@@ -3,13 +3,16 @@
 	background: none;
 	margin-bottom: 0px;
 }
-.breadcrumb i{
-	font-size:1.2rem;
+
+.breadcrumb i {
+	font-size: 1.2rem;
 	font-weight: 600;
 }
-.breadcrumb a{
+
+.breadcrumb a {
 	text-decoration: none;
 }
+
 .general-info {
 	border: 1px solid #0275d8;
 	display: flex;
@@ -66,61 +69,90 @@
 	.general-info {
 		display: none;
 	}
-
-	.general-mobile-view, .more-info-mobile{
+	.general-mobile-view, .more-info-mobile {
 		display: flex;
 		background-color: #fff;
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 50, .04);
-        transition: all .3s ease-in-out;
-        width: 100%;
-        overflow-x: hidden;
+		box-shadow: 0 2px 2px 0 rgba(0, 0, 50, .04);
+		transition: all .3s ease-in-out;
+		width: 100%;
+		overflow-x: hidden;
 		margin-bottom: 10px;
 		margin-top: 10px;
+		font-weight: 600;
 	}
-	.general-mobile-view .school-logo-mobile{
-        margin-right: 10px;
-    }
-	.school-info-mobile{
-        display: block;
-    }
-    .school-info-mobile .school-rating-count{
-        font-weight: 600;
-    	margin-top: 5px;
-    }
-    .school-info-mobile h2{
-        margin-bottom: 0px;
-    	line-height: 1rem;
-    }
-	.school-info-mobile .school-name a{
-        font-size: 1rem;
+	.general-mobile-view .school-logo-mobile {
+		margin-right: 10px;
+	}
+	.school-info-mobile {
+		display: block;
+	}
+	.school-info-mobile .school-rating-count {
+		font-weight: 600;
+		margin-top: 5px;
+	}
+	.school-info-mobile h2 {
+		margin-bottom: 0px;
+		line-height: 1rem;
+	}
+	.school-info-mobile .school-name a {
+		font-size: 1rem;
 		font-weight: 600;
 		text-decoration: none;
-    }
+	}
 	.school-info-mobile .school-rating {
 		position: relative;
 		font-size: 1rem;
-		font-weight: 600; 
+		font-weight: 600;
 	}
-	.school-info-mobile .school-rating i:nth-last-child(even){
-        margin-left: 5px;
+	.school-info-mobile .school-rating i:nth-last-child(even) {
+		margin-left: 5px;
 		margin-right: 5px;
-    }
+	}
+   
 }
 /*hiển thị mobile*/
 @media screen and (min-width: 768px) {
 	.general-mobile-view {
 		display: none;
 	}
-	.more-info-mobile{
-        display: none;
+	.more-info-mobile {
+		display: none;
+	}
+ .button.upload-review {
+        background-color: #f7941d;
+    }
+    .button.is-rounded {
+        border-radius: 0px;
+        padding-left: 1em;
+        padding-right: 1em;
+    	padding-top: 0.5em;
+    	padding-bottom: 0.5em;
+    }
+    .general-info .button-review {
+        position: absolute;
+        box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1);
+        opacity: 0.85;
+        border: none;
+        top: 6rem;
+        right: 1.5rem;
+        transition: 0.2s all;
+    	color: #fff;
+    }
+    .general-info .button-review:hover{
+        background-color: #23d160;
+        border-color: transparent;
+        color: #fff;
+    }
+    .button.is-medium {
+        font-size: 1.25rem;
     }
 }
 </style>
 <div class="container">
 	<nav aria-label="breadcrumb">
 		<ol class="breadcrumb">
-			<li class="breadcrumb-item"><a href="#"><i class="fa fa-home" aria-hidden="true"></i>
-			Trang chủ</a></li>
+			<li class="breadcrumb-item"><a href="#"><i class="fa fa-home"
+					aria-hidden="true"></i> Trang chủ</a></li>
 			<li class="breadcrumb-item active" aria-current="page">Rfgsdgsdgsdfgs</li>
 		</ol>
 	</nav>
@@ -132,8 +164,8 @@
 		</div>
 		<div class="school-info">
 			<h2 class="school-name">
-				<a href="#">Trường Đại Học Ngân Hàng TP. HCM Trường Đại Học Ngân Hàng TP. HCM</a> <span
-					class="school-rating"> <span> <span
+				<a href="#">Trường Đại Học Ngân Hàng TP. HCM Trường Đại Học Ngân
+					Hàng TP. HCM</a> <span class="school-rating"> <span> <span
 						class="icon is-small has-text-warning"> <i
 							class="fa fa-star checked"></i>
 					</span> <span class="icon is-small has-text-warning"> <i
@@ -156,8 +188,9 @@
 					</span>
 				</div>
 				<div class="school-infrastructure">
-					<span class="school-location"> <span class="icon"><i class="fa fa-rss" aria-hidden="true"></i>
-</span> https://fontawesome.com/
+					<span class="school-location"> <span class="icon"><i
+							class="fa fa-rss" aria-hidden="true"></i> </span>
+						https://fontawesome.com/
 					</span>
 				</div>
 				<div class="school-location">
@@ -168,6 +201,11 @@
 				</div>
 			</div>
 		</div>
+		<button
+			class="button-review button is-success is-medium is-rounded upload-review">
+			<span class="icon"> <i class="fa fa-pencil" aria-hidden="true"></i>
+			</span> &nbsp;&nbsp; Viết review
+		</button>
 	</div>
 	<div class="general-mobile-view">
 		<div class="school-logo-mobile">
@@ -177,39 +215,38 @@
 		</div>
 		<div class="school-info-mobile">
 			<h2 class="school-name">
-				<a href="#">Trường Đại Học Ngân Hàng TP. HCM Trường Đại Học Ngân Hàng TP. HCM</a> 
+				<a href="#">Trường Đại Học Ngân Hàng TP. HCM Trường Đại Học Ngân
+					Hàng TP. HCM</a>
 			</h2>
 			<div class="school-rating-count">Reviews - 8</div>
-			<div
-					class="school-rating">Trung bình: <span> <i
-							class="fa fa-star checked"></i><i
-							class="fa fa-star checked"></i><i
-							class="fa fa-star checked"></i><i
-							class="fa fa-star"></i><i
-							class="fa fa-star"></i>
-				</span> 
-				</div>
+			<div class="school-rating">
+				Trung bình: <span> <i class="fa fa-star checked"></i><i
+					class="fa fa-star checked"></i><i class="fa fa-star checked"></i><i
+					class="fa fa-star"></i><i class="fa fa-star"></i>
+				</span>
+			</div>
 		</div>
 	</div>
 	<div class="more-info-mobile">
 		<div class="school-edu">
-				<div class="edu-category">
-					<span class="edu-category"> <span class="icon"><i
-							class="fa fa-graduation-cap" aria-hidden="true"></i></span> Đại
-						học
-					</span>
-				</div>
-				<div class="school-infrastructure">
-					<span class="school-location"> <span class="icon"><i class="fa fa-rss" aria-hidden="true"></i>
-</span> https://fontawesome.com/
-					</span>
-				</div>
-				<div class="school-location">
-					<span class="school-location"> <span class="icon"><i
-							class="fa fa-map-marker" aria-hidden="true"></i></span> 12 Nguyễn
-						Văn Bảo, Phường 4, Gò Vấp, Hồ Chí Minh
-					</span>
-				</div>
+			<div class="edu-category">
+				<span class="edu-category"> <span class="icon"><i
+						class="fa fa-graduation-cap" aria-hidden="true"></i></span> Đại
+					học
+				</span>
 			</div>
+			<div class="school-infrastructure">
+				<span class="school-location"> <span class="icon"><i
+						class="fa fa-rss" aria-hidden="true"></i> </span>
+					https://fontawesome.com/
+				</span>
+			</div>
+			<div class="school-location">
+				<span class="school-location"> <span class="icon"><i
+						class="fa fa-map-marker" aria-hidden="true"></i></span> 12 Nguyễn
+					Văn Bảo, Phường 4, Gò Vấp, Hồ Chí Minh
+				</span>
+			</div>
+		</div>
 	</div>
 </div>
