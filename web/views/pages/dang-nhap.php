1d765728564197e6856ebbd1d1c259ad215ada4a
devcuong <cuong.dh8c@gmail.com>
2020-07-25 21:03:55 +0700

    update

--- web/views/pages/dang-nhap.php
+++ web/views/pages/dang-nhap.php
@@ -31,6 +31,14 @@
 	font-weight: bold;
 	margin-top: 20px;
 }
+
+.btn-block{
+	margin-top: 10px;
+}
+
+.ng-pristine label{
+	margin-bottom: 0px;
+}
 </style>
 <div class="container">
 	<nav aria-label="breadcrumb">
@@ -55,35 +63,24 @@
 						<label>Tài khoản:</label>
 						<div class="input-username">
 							<input
-								class="form-control ng-pristine ng-untouched ng-empty ng-invalid ng-invalid-required"
+								class="form-control"
 								ng-model="userName" id="username" name="username"
-								ng-keypress="($event.charCode==13)?dangNhap():return"
-								placeholder="Tài khoản" required="" type="text"> <span
-								class="clear-text ng-hide"
-								ng-show="userName &amp;&amp; userName.length > 0"
-								ng-click="userName = ''"><i class="fa fa-times"></i></span>
+								placeholder="Tài khoản" required="" type="text">
 						</div>
 						<label>Mật khẩu:</label>
 						<div class="input-password">
 							<input
-								class="form-control ng-pristine ng-untouched ng-empty ng-invalid ng-invalid-required"
+								class="form-control"
 								ng-model="password"
-								ng-keypress="($event.charCode==13)?dangNhap():return"
 								id="password" name="password" placeholder="Mật khẩu" required=""
-								type="password"> <span class="clear-text ng-hide"
-								ng-show="password &amp;&amp; password.length > 0"
-								ng-click="password = ''"><i class="fa fa-times"></i></span> <span
-								ng-click="showPassword = !showPassword"><i class="fa fa-eye"></i></span>
+								type="password">
 						</div>
+						<div></div>
 					</form>
 					<div class="text-center">
-						<!-- ngIf: thongBao -->
 					</div>
-					<button class="btn btn-primary btn-block" ng-click="dangNhap()">
-						<!-- ngIf: !spinner -->
+					<button class="btn btn-primary btn-block">
 						<span ng-if="!spinner" class="ng-scope">Đăng nhập</span>
-						<!-- end ngIf: !spinner -->
-						<!-- ngIf: spinner -->
 					</button>
 					<div class="text-center">
 						<a class="btn btn-link" id="forget-password" href="/quen-mat-khau">Quên
